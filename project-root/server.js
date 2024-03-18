432aef82d1f737821ed81b15af901d1d3979ec00
EricKart <itsary1020@yahoo.com>
2024-03-18 07:26:55 +0530

    Single push for complete RESTAPI

---
+++ project-root/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const routes = require('./src/routes/index');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+app.use(express.json());
+
+app.use(bodyParser.json());
+app.use('/api', routes);
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
